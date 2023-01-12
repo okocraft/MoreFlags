f86a5211a99a28717f7cea2b69b19f6885b45a55
LazyGon <26216029+LazyGon@users.noreply.github.com>
2023-01-12 13:39:44 +0900

    fix: fix many bugs found on debugging in game. - fix bug that raiders still spawn even if the raid is stopped by flag changing - fix bug that players get hero effect even if the raid is stopped by flag changing - fix bug that players are granted hero advancement even if the raid is stopped by flag changing - apply worldguard bypass state of raid cause player

--- src/main/java/net/okocraft/moreflags/listener/RaidListener.java
+++ src/main/java/net/okocraft/moreflags/listener/RaidListener.java
@@ -1,56 +1,172 @@
 package net.okocraft.moreflags.listener;
 
+import com.sk89q.worldedit.bukkit.BukkitAdapter;
+import com.sk89q.worldedit.world.World;
+import com.sk89q.worldguard.LocalPlayer;
+import com.sk89q.worldguard.WorldGuard;
+import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
 import com.sk89q.worldguard.protection.flags.StateFlag;
-import java.util.List;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
 import net.okocraft.moreflags.CustomFlags;
 import net.okocraft.moreflags.Main;
 import net.okocraft.moreflags.util.FlagUtil;
+import org.bukkit.Location;
+import org.bukkit.NamespacedKey;
+import org.bukkit.Raid;
+import org.bukkit.Statistic;
+import org.bukkit.advancement.Advancement;
+import org.bukkit.advancement.AdvancementProgress;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
-import org.bukkit.entity.Raider;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.raid.RaidEvent;
+import org.bukkit.event.entity.EntityPotionEffectEvent;
+import org.bukkit.event.player.PlayerStatisticIncrementEvent;
 import org.bukkit.event.raid.RaidFinishEvent;
 import org.bukkit.event.raid.RaidSpawnWaveEvent;
+import org.bukkit.event.raid.RaidStopEvent;
 import org.bukkit.event.raid.RaidTriggerEvent;
+import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.potion.PotionEffectType;
+import org.jetbrains.annotations.Nullable;
 
 public class RaidListener implements Listener {
 
+    public static final String CANCEL_HERO_META_KEY = "cancel_hero";
+
     private final Main plugin;
+    private final FixedMetadataValue cancelHeroMeta;
+    private final Advancement heroOfTheVillage;
+
+    private final Map<Integer, UUID> raidIdCauseMap = new HashMap<>();
 
     public RaidListener(Main plugin) {
         this.plugin = plugin;
+        this.cancelHeroMeta = new FixedMetadataValue(plugin, null);
+        this.heroOfTheVillage = plugin.getServer()
+                .getAdvancement(NamespacedKey.minecraft("adventure/hero_of_the_village"));
     }
 
     @EventHandler(ignoreCancelled = true)
     private void onRaidTrigger(RaidTriggerEvent event) {
-        if (!canRaid(event)) {
+        if (!canRaid(event.getRaid())) {
             event.setCancelled(true);
-        }
-    }
-
-
-    @EventHandler(ignoreCancelled = true)
-    private void onRaidSpawnWave(RaidSpawnWaveEvent event) {
-        if (!canRaid(event)) {
-            event.getRaiders().forEach(Raider::remove);
+        } else {
+            raidIdCauseMap.put(getRaidId(event.getRaid()), event.getPlayer().getUniqueId());
         }
     }
 
     @EventHandler
-    private void onRaid(RaidFinishEvent event) {
-        if (!canRaid(event)) {
-            List<Player> winners = event.getWinners();
-            plugin.getServer().getScheduler().runTask(
-                    plugin,
-                    () -> winners.forEach(p -> p.removePotionEffect(PotionEffectType.HERO_OF_THE_VILLAGE))
-            );
+    private void onRaidSpawnWave(RaidSpawnWaveEvent event) {
+        if (!canRaid(event.getRaid())) {
+            // DON'T USE event.getRaiders(). IT'S BROKEN.
+            event.getRaid().getRaiders().forEach(Entity::remove);
         }
     }
 
-    private boolean canRaid(RaidEvent event) {
-        StateFlag.State s = FlagUtil.queryValue(event.getWorld(), event.getRaid().getLocation(), null, CustomFlags.RAID);
-        return s != null && s != StateFlag.State.DENY;
+    @EventHandler
+    private void onRaidFinish(RaidFinishEvent event) {
+        if (!canRaid(event.getRaid())) {
+            event.getWinners().forEach(p -> p.getPotionEffect(PotionEffectType.HERO_OF_THE_VILLAGE));
+        }
+    }
+
+    @EventHandler
+    private void onRaidStop(RaidStopEvent event) {
+        raidIdCauseMap.remove(getRaidId(event.getRaid()));
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    private void onEntityPotionEffect(EntityPotionEffectEvent event) {
+        if (event.getCause() != EntityPotionEffectEvent.Cause.PATROL_CAPTAIN) {
+            return;
+        }
+        if (!(event.getEntity() instanceof Player player)) {
+            return;
+        }
+        Raid raid = getParticipatingRaid(player);
+        if (raid != null && !canRaid(raid)) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
+    private void onPlayerStatisticIncrement(PlayerStatisticIncrementEvent event) {
+        if (event.getStatistic() != Statistic.RAID_WIN) {
+            return;
+        }
+        Raid raid = getParticipatingRaid(event.getPlayer());
+        if (raid == null || canRaid(raid)) {
+            return;
+        }
+
+        event.getPlayer().setMetadata(CANCEL_HERO_META_KEY, cancelHeroMeta);
+        event.setCancelled(true);
+
+        AdvancementProgress ap = event.getPlayer().getAdvancementProgress(heroOfTheVillage);
+        Set<String> current = new HashSet<>(ap.getAwardedCriteria());
+
+        plugin.getServer().getScheduler().runTask(plugin, () -> {
+            Set<String> awarded = new HashSet<>(ap.getAwardedCriteria());
+            if (!awarded.equals(current)) {
+                awarded.removeAll(current);
+                awarded.forEach(ap::revokeCriteria);
+                plugin.getServer().getConsoleSender().sendMessage("The completion of challenge has been cancelled.");
+            }
+            event.getPlayer().removeMetadata(CANCEL_HERO_META_KEY, plugin);
+        });
+    }
+
+    @Nullable
+    public static Raid getParticipatingRaid(Player player) {
+        return player.getWorld().getRaids().stream()
+                .filter(r -> r.getHeroes().contains(player.getUniqueId()))
+                .findAny()
+                .orElse(null);
+    }
+
+    private boolean canRaid(Raid raid) {
+        LocalPlayer subject;
+        Location raidPos = raid.getLocation();
+
+        UUID causePlayerId = raidIdCauseMap.get(getRaidId(raid));
+        if (causePlayerId == null) {
+            subject = null;
+        } else {
+            subject = WorldGuardPlugin.inst().wrapPlayer(plugin.getServer().getPlayer(causePlayerId));
+            World weWorld = BukkitAdapter.adapt(raidPos.getWorld());
+            if (WorldGuard.getInstance().getPlatform().getSessionManager().hasBypass(subject, weWorld)) {
+                return true;
+            }
+        }
+
+        return FlagUtil.queryValue(raidPos.getWorld(), raidPos, subject, CustomFlags.RAID) == StateFlag.State.ALLOW;
+    }
+
+    private static Field HANDLE_FIELD;
+    private static Method GET_ID_METHOD;
+
+    private static int getRaidId(Raid raid) {
+        try {
+            if (HANDLE_FIELD == null) {
+                HANDLE_FIELD = raid.getClass().getDeclaredField("handle");
+                HANDLE_FIELD.setAccessible(true);
+            }
+            HANDLE_FIELD.setAccessible(true);
+            Object handle = HANDLE_FIELD.get(raid);
+            if (GET_ID_METHOD == null) {
+                GET_ID_METHOD = handle.getClass().getMethod("getId");
+            }
+            return (int) GET_ID_METHOD.invoke(handle);
+        } catch (ReflectiveOperationException e) {
+            return -1;
+        }
     }
 }
--- src/main/java/net/okocraft/moreflags/protocollib/ClientboundPacketListener.java
+++ src/main/java/net/okocraft/moreflags/protocollib/ClientboundPacketListener.java
@@ -3,19 +3,25 @@ package net.okocraft.moreflags.protocollib;
 import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.events.PacketAdapter;
 import com.comphenix.protocol.events.PacketEvent;
+import com.comphenix.protocol.wrappers.Converters;
+import com.comphenix.protocol.wrappers.MinecraftKey;
 import com.sk89q.worldguard.LocalPlayer;
 import com.sk89q.worldguard.WorldGuard;
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import java.util.Map;
+import java.util.Set;
 import net.kyori.adventure.text.TranslatableComponent;
 import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer;
 import net.okocraft.moreflags.Main;
 import net.okocraft.moreflags.listener.DeathMessageListener;
+import net.okocraft.moreflags.listener.RaidListener;
 import org.bukkit.entity.Player;
 
 public class ClientboundPacketListener extends PacketAdapter {
 
     private static final PacketType[] PACKET_TYPES_TO_LISTEN = {
-            PacketType.Play.Server.SYSTEM_CHAT
+            PacketType.Play.Server.SYSTEM_CHAT,
+            PacketType.Play.Server.ADVANCEMENTS
     };
 
     ClientboundPacketListener(Main plugin) {
@@ -36,6 +42,15 @@ public class ClientboundPacketListener extends PacketAdapter {
                     event.getPacket().getBooleans().read(0),
                     event
             );
+        } else if (event.getPacketType() == PacketType.Play.Server.ADVANCEMENTS) {
+            onUpdateAdvancements(
+                    event.getPlayer(),
+                    event.getPacket().getBooleans().read(0),
+                    event.getPacket().getMaps(MinecraftKey.getConverter(), Converters.passthrough(null)).read(0),
+                    event.getPacket().getSets(MinecraftKey.getConverter()).read(0),
+                    event.getPacket().getMaps(MinecraftKey.getConverter(), Converters.passthrough(null)).read(1),
+                    event
+            );
         }
     }
 
@@ -43,14 +58,38 @@ public class ClientboundPacketListener extends PacketAdapter {
         if (content == null) {
             return;
         }
-        LocalPlayer lp = WorldGuardPlugin.inst().wrapPlayer(client);
-        if (WorldGuard.getInstance().getPlatform().getSessionManager().hasBypass(lp, lp.getWorld())) {
+
+        if (!(GsonComponentSerializer.gson().deserialize(content) instanceof TranslatableComponent translatable)) {
             return;
         }
 
-        if (GsonComponentSerializer.gson().deserialize(content) instanceof TranslatableComponent translatable
-                && translatable.key().startsWith("death.")
-                && client.hasMetadata(DeathMessageListener.METADATA_KEY)) {
+        if (translatable.key().startsWith("death.") && client.hasMetadata(DeathMessageListener.METADATA_KEY)) {
+            LocalPlayer lp = WorldGuardPlugin.inst().wrapPlayer(client);
+            if (!WorldGuard.getInstance().getPlatform().getSessionManager().hasBypass(lp, lp.getWorld())) {
+                event.setCancelled(true);
+            }
+            return;
+        }
+
+        if (translatable.key().equals("chat.type.advancement.challenge")
+                && translatable.args().size() >= 2
+                && translatable.args().get(1) instanceof TranslatableComponent bracket
+                && bracket.key().equals("chat.square_brackets")
+                && bracket.args().size() >= 1
+                && bracket.args().get(0) instanceof TranslatableComponent title
+                && title.key().equals("advancements.adventure.hero_of_the_village.title")
+                && client.hasMetadata(RaidListener.CANCEL_HERO_META_KEY)) {
+            event.setCancelled(true);
+        }
+
+    }
+
+    private void onUpdateAdvancements(Player client, boolean reset, Map<MinecraftKey, Object> added, Set<MinecraftKey> removed, Map<MinecraftKey, Object> progress, PacketEvent event) {
+        if (added.keySet().stream().noneMatch(k -> k.getKey().contains("hero_of_the_village"))) {
+            return;
+        }
+
+        if (client.hasMetadata(RaidListener.CANCEL_HERO_META_KEY)) {
             event.setCancelled(true);
         }
     }
