15701e7e5d893a135cf42ec9b2b141abe7176029
Siroshun09 <siro.shun8@gmail.com>
2024-05-12 20:55:15 +0900

    build: update ProtocolLib to 5.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ repositories {
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
     compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.9-SNAPSHOT")
-    compileOnly("com.comphenix.protocol:ProtocolLib:5.0.0")
+    compileOnly("com.comphenix.protocol:ProtocolLib:5.1.0")
 }
 
 tasks.compileJava {
